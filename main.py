ac207e18db9c0f9ee9eef491ff786069b469a451
Alex Zyner <a.zyner@acfr.usyd.edu.au>
2017-01-11 17:06:17 +1100

    Added Naturalistic Dataset Importer

--- main.py
+++ main.py
@@ -10,3 +10,14 @@
 # start cross fold loops
 #   instance batch handler
 #
+import intersection_segments
+import sys
+import copy
+import numpy as np
+import pandas as pd
+
+
+input_columns = ['easting', 'northing', 'heading', 'speed']
+raw_sequences, raw_classes = intersection_segments.get_manouvre_sequences(input_columns)
+
+print raw_sequences
