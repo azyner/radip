867c8724b7fec3466f6ab10ced6f34eb6c2fdc3d
Alex Zyner <a.zyner@acfr.usyd.edu.au>
2017-01-18 13:46:03 +1100

    Caught bug in BatchHandler, padding vector invalid
    To be investigated

--- UnitTests/test_batchHandler.py
+++ UnitTests/test_batchHandler.py
@@ -21,8 +21,10 @@ class TestBatchHandler(TestCase):
 
         Wrangler.split_into_evaluation_pools()
         cf_pool, test_pool = Wrangler.get_pools()
+        cf_fold = -1
 
         for train_pool, val_pool in cf_pool:
+            cf_fold += 1
             training_batch_handler = BatchHandler.BatchHandler(train_pool,parameters.parameters,True)
             validation_batch_handler = BatchHandler.BatchHandler(val_pool,parameters.parameters,False)
 
@@ -30,15 +32,27 @@ class TestBatchHandler(TestCase):
 
             if len(train_x[0]) != parameters.parameters['batch_size']:
                 self.fail()
-            # Get number of unique tracks in val pool
-            num_val_tracks = len(val_pool['track_idx'].unique())
-            validation_batch_handler.set_distance_threshold(d_thresh=22)
-            complete = False
-            pad_array = []
-            while not complete:
-                X,Y,weights,pad,complete =validation_batch_handler.get_minibatch()
-                pad_array.extend(pad)
-            if (sum(np.array(weights[0]).astype(int))) != num_val_tracks:
-                self.fail()
-
+            if cf_fold ==0:
+                # Get number of unique tracks in val pool
+                num_val_tracks = len(val_pool['track_idx'].unique())
+                validation_batch_handler.set_distance_threshold(d_thresh=22)
+                complete = False
+                pad_array = []
+                while not complete:
+                    X,Y,weights,pad,complete =validation_batch_handler.get_minibatch()
+                    pad_array.extend(pad)
+                if (sum(np.array(weights[0]).astype(int))) != num_val_tracks:
+                    self.fail()
+            if cf_fold ==1:
+                # Get number of unique tracks in val pool
+                num_samples = len(val_pool)
+                complete = False
+                pad_array = []
+                total_valid = 0
+                while not complete:
+                    X, Y, weights, pad, complete = validation_batch_handler.get_minibatch()
+                    total_valid += np.sum(np.logical_not(pad)*1)
+                    pad_array.extend(pad)
+                if total_valid != num_samples:
+                    self.fail()
 
