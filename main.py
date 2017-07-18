f497b9a989f72ac14e9cb2d7890427a76848893f
Alex Zyner <a.zyner@acfr.usyd.edu.au>
2017-07-18 17:29:58 +1000

    Now it grabs the parameters from inside the checkpoint file

--- main.py
+++ main.py
@@ -14,7 +14,7 @@
 import TrainingManager
 import intersection_segments
 import SequenceWrangler
-import parameters
+
 import datetime
 import os
 import pandas as pd
@@ -27,16 +27,35 @@ import pickle
 # This is where the hyperparameter searcher goes
 
 # ibeoCSV = ibeoCSVImporter.ibeoCSVImporter(parameters,'data/20170601-stationary-3-leith-croydon.csv')
-#checkpoint_dir = "network_plots/20170717-184233/train/best-1500280960.45"
+checkpoint_dir = "network_plots/20170718-164817/train/best-1500360501.23"
+#checkpoint_dir = None
+if 'checkpoint_dir' in locals():
+    test_network_only = True
+else:
+    checkpoint_dir = None
+    test_network_only = False
 
-checkpoint_dir = None
-test_network_only = False
 
 if checkpoint_dir is not None:
     print "Loading from checkpoint..."
-    parameters.parameters['master_dir'] = os.path.abspath(os.path.join(os.path.join(checkpoint_dir,os.pardir),os.pardir))
+    # The parameters file in the checkpoint master folder must be imported for things like obs length, etc
+    master_dir = os.path.relpath(os.path.join(os.path.join(checkpoint_dir,os.pardir),os.pardir))
+
+    def touch(fname):
+        try:
+            os.utime(fname, None)
+        except OSError:
+            open(fname, 'a').close()
+    touch(os.path.join(master_dir, "__init__.py"))
+    touch(os.path.join(os.path.join(master_dir,os.pardir), "__init__.py"))
+
+    import importlib
+    import_name = master_dir + ".parameters"
+    parameters = importlib.import_module(import_name.replace('/','.'))
+    parameters.parameters['master_dir'] = master_dir
 
 if checkpoint_dir is None:
+    import parameters
     results_dir = 'results'
     if not os.path.exists(results_dir):
         os.makedirs(results_dir)
@@ -46,13 +65,13 @@ if checkpoint_dir is None:
     shutil.copy("parameters.py",os.path.join(parameters.parameters['master_dir'],"parameters.py"))
     print "results folder made, parameter file copied"
 
-githash = subprocess.check_output(["git", "describe", "--always"])
-with open(os.path.join(parameters.parameters['master_dir'], githash + ".githash"), "w") as outfile:
-    outfile.write("Git hash:")
-    outfile.write(githash)
-    outfile.write("Git diff:")
-    diff = subprocess.check_output(["git", "diff"])
-    outfile.write(diff)
+    githash = subprocess.check_output(["git", "describe", "--always"])
+    with open(os.path.join(parameters.parameters['master_dir'], githash + ".githash"), "w") as outfile:
+        outfile.write("Git hash:")
+        outfile.write(githash)
+        outfile.write("Git diff:")
+        diff = subprocess.check_output(["git", "diff"])
+        outfile.write(diff)
 
 print "wrangling tracks"
 
@@ -85,12 +104,18 @@ else:
         print "reading data and splitting into data pool, this will take some time (10? minutes). Grab a coffee"
         raw_sequences, raw_classes = intersection_segments.get_manouvre_sequences(parameters.parameters['input_columns'])
         Wrangler.generate_master_pool_naturalistic_2015(raw_sequences, raw_classes)
-
-Wrangler.split_into_evaluation_pools()
+if checkpoint_dir is not None:
+    # Guarantee that the tracks used for testing are the same as during training
+    with open(os.path.join(parameters.parameters['master_dir'], 'data.pkl'), 'rb') as pkl_file:
+        from_pickle = pickle.load(pkl_file)
+        Wrangler.split_into_evaluation_pools(test_idxs=from_pickle['test_idxs'], trainval_idxs=from_pickle['trainval_idxs'])
+else:
+    Wrangler.split_into_evaluation_pools()
 cf_pool, test_pool = Wrangler.get_pools()
 
 to_pickle = {}
-to_pickle['test_idxs'] = test_pool.track_idx.unique()
+to_pickle['test_idxs'] = Wrangler.test_idxs
+to_pickle['trainval_idxs'] = Wrangler.trainval_idxs
 to_pickle['data_pool'] = Wrangler.get_pool_filename()
 
 with open(os.path.join(parameters.parameters['master_dir'],'data.pkl'),'wb') as pkl_file:
@@ -112,7 +137,6 @@ ideas = None
 #Anything else to pickle?
 # I need the track idx's for test train split for the visualiser
 
-ideas = None
 # Select best model based on hyper parameters
 # Train on all training/val data
 # Run on test data
