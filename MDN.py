Fixed scaling and heading thresholding on loopback computation

MDN.py
@@ -165,14 +165,16 @@ def compute_derivates(output_prev, output_current, network_input_columns,
     pos_d = tf.abs(pos_d_i)  # Use abs to get magnitude
     print "Warning, velocity loopback generator assumes 25 Hz timesteps"
     v_c = tf.multiply(pos_d,25) # delta * 25 = number of meters per second
-    h_c = tf.atan2(tf.subtract(x_p,x_c),tf.subtract(y_p, y_c))
+    #For whatever reason, atan2 convention is atan2(y,x)
+    h_c = tf.atan2(tf.subtract(y_c, y_p), tf.subtract(x_c,x_p))
     # TODO Element wise, I have to condition on speed. If < 2m/s (hyperparameter?) use old heading, else compute heading
     # I don't want to use tf.cond as it does not perform element-wise logic.
     # So I'm going to construct this fundamentally - MUltiply by zero or one and sum
-    use_old_speed = tf.less(v_c, velocity_threshold) # Broadcasting will upsize the scalar to a vector
-    use_new_speed = tf.logical_not(use_old_speed)
-    use_old_speed, use_new_speed = (tf.to_float(use_old_speed),tf.to_float(use_new_speed))
-    new_speed = tf.add(tf.multiply(use_old_speed,speed_p), tf.multiply(use_new_speed,v_c))
-    output_with_extras = tf.concat([x_c,y_c,h_c,new_speed],axis=1)
+    use_old_heading = tf.less(v_c, velocity_threshold) # Broadcasting will upsize the scalar to a vector
+    use_new_heading = tf.logical_not(use_old_heading)
+    use_old_heading, use_new_heading = (tf.to_float(use_old_heading),tf.to_float(use_new_heading))
+    # This makes no sense. I was supposed to threshold heading on speed, not the other way around.
+    new_heading = tf.add(tf.multiply(use_old_heading,heading_p), tf.multiply(use_new_heading,h_c))
+    output_with_extras = tf.concat([x_c,y_c,new_heading,v_c],axis=1)
 
     return output_with_extras