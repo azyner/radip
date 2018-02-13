6ee23a7ada7902b7e8794a5a40c05c3fdb0afdf4
Alex Zyner <a.zyner@acfr.usyd.edu.au>
2018-02-13 13:10:33 +1100

    Added distance metrics, Modified Hausdorff Distance

--- ReportWriter.py
+++ ReportWriter.py
@@ -1,4 +1,6 @@
 from scipy.spatial import distance
+import numpy as np
+from numpy.core.umath_tests import inner1d
 
 class ReportWriter:
     def __init__(self,
@@ -13,16 +15,39 @@ class ReportWriter:
     # A) metric variance. LCSS, Hausdorff, etc
     # B) Statistical variance:
         # best mean
-        # best worst 5% / 1% / 0.1% <-- It tooke me ages to get dat for a reasonable 0.1% fit!
+        # best worst 5% / 1% / 0.1% <-- It took me ages to get data for a reasonable 0.1% fit!
     def _score_model_on_metric(self, report_df, metric=None):
-        error = 0
+        scores_list = []
         for track in report_df.iterrows():
+            track_scores = {}
             track = track[1]
-            preds = track.outputs
-            gts = track.decoder_sample
 
-
+            preds = track.outputs[np.logical_not(track.trackwise_padding)]
+            gts = track.decoder_sample[np.logical_not(track.trackwise_padding)]
+            #TODO Trim tracks here based on trackwise padding.
+            ### EUCLIDEAN ERROR -- Average
+            euclid_error = []
             for pred, gt in zip(preds[:,0:2], gts[:,0:2]):
-                error += distance.euclidean(pred, gt)
-            ideas = None
-        return error
+                # Iterates over each time-step
+                euclid_error.append(distance.euclidean(pred, gt))
+
+            ### MODIFIED HAUSDORFF DISTANCE
+            # Pulled shamelessly from https://github.com/sapphire008/Python/blob/master/generic/HausdorffDistance.py
+            # Thanks sapphire008!
+            (A, B) = (preds[:, 0:2], gts[:, 0:2])
+            # Find pairwise distance
+            D_mat = np.sqrt(inner1d(A, A)[np.newaxis].T +
+                            inner1d(B, B) - 2 * (np.dot(A, B.T)))
+            # Calculating the forward HD: mean(min(each col))
+            FHD = np.mean(np.min(D_mat, axis=1))
+            # Calculating the reverse HD: mean(min(each row))
+            RHD = np.mean(np.min(D_mat, axis=0))
+            # Calculating mhd
+            MHD = np.max(np.array([FHD, RHD]))
+
+            track_scores['euclidean'] = np.mean(np.array(euclid_error))
+            track_scores['MHD'] = MHD
+
+
+            scores_list.append(track_scores)
+        return scores_list
